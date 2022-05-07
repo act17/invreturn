13044b91b00b0b6efc44ffd8625230a9351c5d94
Aiden Taylor <91446658+act17@users.noreply.github.com>
2022-05-07 11:52:41 -0600

    Updated to Version 2.5
    See README.md for details.

--- interesttools.h
+++ interesttools.h
@@ -1,7 +1,8 @@
 #ifndef _INTERESTTOOLS_H
 #define _INTERESTTOOLS_H
 
-
 double interestcalc(double investment, double rate, double period, double week);
+double interestfind(double benchmark, double investment, double rate, double period);
+int weekfind(double week);
 
 #endif
