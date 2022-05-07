ff0c849779bcb53ce1989ee52b9cccd63519aa55
Aiden Taylor <91446658+act17@users.noreply.github.com>
2022-05-07 11:53:12 -0600

    Updated to Version 2.5.
    See README.md for details.

--- interesttools.c
+++ interesttools.c
@@ -6,3 +6,20 @@ double interestcalc(double investment, double rate, double period, double week)
 	return investment * pow(rate, week / period);	//Thanks to 'rene' on the Linux Mint Forums for providing this simplified
 							//function.
 }
+
+double interestfind(double benchmark, double investment, double rate, double period)
+{
+	return ((log(benchmark/investment)/log(rate)) * period);
+}
+
+int weekfind(double week){
+	int weekcount = 1;
+	while(1){
+		int weekcheck = week - (33 * weekcount);
+		if(weekcheck < 34)
+			break;
+		else
+			weekcount++;
+	}
+	return weekcount + 1;
+}
